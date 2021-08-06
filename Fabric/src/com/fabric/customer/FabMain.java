26f007252621eb88c474cf980bd9b980c5706690
GAYATHRI <sai15gayathri@gmail.com>
2021-08-06 20:55:26 +0530

    Final commit

--- Fabric/src/com/fabric/customer/FabMain.java
+++ Fabric/src/com/fabric/customer/FabMain.java
@@ -3,37 +3,40 @@ package com.fabric.customer;
 import com.fabric.check.FabricCheck;
 import com.fabric.exception.*;
 
+
 public class FabMain {
 
-	public static void main(String[] args)  {
-		FabricCheck fcheck=new FabricCheck();
-		
-		try {
+	public static void main(String[] args) {
+		FabricCheck fcheck = new FabricCheck();
+
+		try 
+		{
 			System.out.println(fcheck.getByColor("Yellow"));
-		} catch (ColorNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
-		}
-		try {
+		try 
+		{
 			System.out.println(fcheck.getByPrice(800));
-		} catch (FabricNotAvailableException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
+			try {
+				System.out.println(fcheck.getByFname("Rayon"));
+				try {
+					System.out.println(fcheck.getById(8));
+				}
+				catch(IdNotFoundException e) {
+					System.out.println(e.getMessage());
+				}
+			}
+			catch(FabricNotAvailableException e2) {
+				System.out.println(e2.getMessage());
+			}
 		}
-		try {
-			System.out.println(fcheck.getByFname("Rayon"));
-		} catch (FabricNotAvailableException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
+		catch(FabricNotAvailableException e3) {
+			System.out.println(e3.getMessage());
 		}
-		try {
-			System.out.println(fcheck.getById(8));
-		} catch (IdNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
 		}
-
-
+		catch(ColorNotFoundException e4)
+		{
+			System.out.println(e4.getMessage());
+		}
 	}
+	
 
 }
